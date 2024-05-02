e142abf796b888d6c8d20bb32198bbc603975db9
Sergey Petrushin <s.petrushin128@gmail.com>
2024-05-03 02:16:56 +0400

    migrate to sqlite

--- Models/UserManga.py
+++ Models/UserManga.py
@@ -1,9 +1,8 @@
 from application import db
-from sqlalchemy.dialects.postgresql import ARRAY
 
 
 class UserManga(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    cart = db.Column(ARRAY(db.Integer), default=[])
-    favourite_manga = db.Column(ARRAY(db.Integer), default=[])
-    purchased_manga = db.Column(ARRAY(db.Integer), default=[])
+    cart = db.Column(db.JSON, default={"cart": []})
+    favourite_manga = db.Column(db.JSON, default={"favourite_manga": []})
+    purchased_manga = db.Column(db.JSON, default={"purchased_manga": []})
